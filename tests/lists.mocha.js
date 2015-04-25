715d7a85362e93a30c9d4bf52753bf9fe89fab2f
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-04-25 15:38:50 -0500

    npm maintenance

--- tests/lists.mocha.js
+++ tests/lists.mocha.js
@@ -1,7 +1,7 @@
 
 require('must');
 require('owski-primitives').mport(function(add){
-require('owski-lists').mport(function(each,map,head,eachOwn,rest,reduceStrings,reverse){
+require('../lists').mport(function(each,map,head,eachOwn,rest,reduceStrings,reverse){
   describe('Lists',function(){
     it('map should work for arrays',function(){
       map(function(i){
